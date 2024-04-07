6fe582e88ec8667a0f149c4dda228d070f010930
Bikal Chudal <bikalchudal11@gmail.com>
2024-04-07 19:31:00 +0545

    Day 1 completed with homework

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:day1/calculator/caculator_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,8 +12,47 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, home: CalculatorPage());
   }
 }
+
+
+// Scaffold(
+//         appBar: AppBar(
+//           foregroundColor: Colors.white,
+//           leading: Icon(
+//             Icons.line_axis,
+//           ),
+//           backgroundColor: Colors.deepPurple,
+//           title: Text(
+//             "Hello There",
+//             style: TextStyle(
+//               fontSize: 25,
+//             ),
+//           ),
+//           centerTitle: true,
+//           actions: [
+//             Icon(Icons.new_label),
+//             Icon(Icons.duo),
+//             Icon(Icons.duo),
+//           ],
+//         ),
+//         body: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Text("This is Body"),
+//             Text("This is Body"),
+//             Text("This is Body"),
+//             Text("This is Body"),
+//             Row(
+//               children: [
+//                 Icon(Icons.add),
+//                 Icon(Icons.add),
+//                 Icon(Icons.add),
+//                 Icon(Icons.add),
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
